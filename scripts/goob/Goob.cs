200329a179c8ad35f50a4f127086d8e65790f2d6
carsonuderitz@gmail.com <carsonuderitz@gmail.com>
2025-08-29 12:58:37 -0400

    he walk

--- scripts/goob/Goob.cs
+++ scripts/goob/Goob.cs
@@ -11,7 +11,35 @@ public partial class Goob : Node2D
 		.Goal(testGoal)
 		.Build();
 
+	[Export] private float speed;
+	[Export] private float distanceThreshold = 2.0f;
+
+	private NavigationAgent2D navAgent;
+	private Sprite2D sprite;
+	private AnimationPlayer animPlayer;
+
 	public override void _Ready()
 	{
+		navAgent = GetNode<NavigationAgent2D>("NavigationAgent2D");
+		sprite = GetNode<Sprite2D>("GooberSprite");
+		animPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+	}
+
+	public override void _Process(double delta)
+	{
+		navAgent.TargetPosition = GetGlobalMousePosition();
+
+		if (Position.DistanceTo(navAgent.TargetPosition) > distanceThreshold)
+		{
+			Vector2 dir = Position.DirectionTo(navAgent.GetNextPathPosition());
+			Position += dir * speed * (float)delta;
+
+			sprite.FlipH = dir.X < 0;
+			animPlayer.Play("move");
+		}
+		else
+		{
+			animPlayer.Stop();
+		}
 	}
 }
